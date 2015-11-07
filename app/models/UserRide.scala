c7eaddee0ce32ab93c7a2b7204895ad3906609e6
Michael Dell <apfelbaum24@gmail.com>
2015-11-07 15:01:48 +0100

    user rides model

---
+++ app/models/UserRide.scala
@@ -0,0 +1,20 @@
+package models
+
+import java.sql.Timestamp
+
+import org.joda.time.DateTime
+
+/**
+  * Created by michi on 07/11/15.
+  */
+case class UserRide(user: String, train: String, start: StartStopStamp, stopStamp: StartStopStamp)
+
+case class StartStopStamp(timestamp: DateTime, geo: Geo)
+
+case class Geo(lat: Long, long: Long)
+
+object UserRides {
+
+  var userRides = Seq[UserRide]()
+
+}
