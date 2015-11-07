8cc52cf67d356eecc576f56893a7939169ee9d28
Michael Dell <apfelbaum24@gmail.com>
2015-11-07 15:30:37 +0100

    Save enter leave events inmemory

--- app/controllers/AppController.scala
+++ app/controllers/AppController.scala
@@ -1,5 +1,7 @@
 package controllers
 
+import models.{EnterLeaveEvents, EnterLeaveEvent}
+import play.api.Logger
 import play.api.libs.json.{JsError, Json}
 import play.api.mvc._
 
@@ -8,14 +10,7 @@ import play.api.mvc._
   */
 class AppController extends Controller with EventTrigger {
 
-  /**
-    *   {
-    beaconId: "asdsad"
-    userId: "asdas"
-    timestamp: "234324234"
-  }
-    */
-  case class EnterLeaveEvent(beaconId: String, userId: String, timestamp: String)
+
   //JSON Macro Inception
   implicit val jsonWrites = Json.writes[EnterLeaveEvent]
   implicit val jsonReads = Json.reads[EnterLeaveEvent]
@@ -28,6 +23,8 @@ class AppController extends Controller with EventTrigger {
         BadRequest(Json.obj("status" ->"KO", "message" -> JsError.toFlatJson(errors)))
       },
       event => {
+        EnterLeaveEvents.enterLeaveEvents += event
+        Logger.debug(EnterLeaveEvents.enterLeaveEvents.toString())
         Ok(Json.obj("status" ->"OK", "message" -> ("Event "+event.toString+" saved.") ))
       }
     )
@@ -41,6 +38,7 @@ class AppController extends Controller with EventTrigger {
         BadRequest(Json.obj("status" ->"KO", "message" -> JsError.toFlatJson(errors)))
       },
       event => {
+        EnterLeaveEvents.enterLeaveEvents += event
         Ok(Json.obj("status" ->"OK", "message" -> ("Event "+event.toString+" saved.") ))
       }
     )
